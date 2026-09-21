1473e95f354f26d9ad86b0497e62251642d9fedf
Le Van Si <sixlevan@gmail.com>
2026-09-21 13:03:37 +0700

    feat(w02): complete task-007 operators and typeof

---
+++ builder-labs/w02-operators.js
@@ -0,0 +1,21 @@
+let thiNghiem1 = {
+    a: 20,
+    b: "5",
+};
+console.log('ket qua cua T1 (+): ', thiNghiem1.a + thiNghiem1.b, typeof (thiNghiem1.a + thiNghiem1.b));
+console.log('ket qua cua T1 (-): ', thiNghiem1.a - thiNghiem1.b, typeof (thiNghiem1.a - thiNghiem1.b));
+
+let thiNghiem2 = {
+    a: 5,
+    b: "5",
+}
+console.log('ket qua cua T2 (==): ', thiNghiem2.a == thiNghiem2.b);
+console.log('ket qua cua T2 (===): ', thiNghiem2.a === thiNghiem2.b);
+
+let thiNghiem3 = {
+    a: 17,
+    b: 5,
+    c: 2,
+}
+console.log('ket qua cua T3 (%): ', thiNghiem3.a % thiNghiem3.b);
+console.log('ket qua cua T3 (**): ', thiNghiem3.c ** 3);
