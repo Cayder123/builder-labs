4c3946f0fe7e629254f9f95fc1a7258740188617
Le Van Si <sixlevan@gmail.com>
2026-09-21 10:18:29 +0700

    feat(w02): complete task-006 primitive types

---
+++ builder-labs/w02-primitive-types.js
@@ -0,0 +1,15 @@
+let address;
+
+let User_Profile = {
+    tenNguoiDung: "Le Van Si",
+    tuoi: 21,
+    daDilam: false,
+    nguoiYeu: null,
+    address: address,
+};
+
+console.log(typeof User_Profile.tenNguoiDung);
+console.log(typeof User_Profile.tuoi);
+console.log(typeof User_Profile.daDilam);
+console.log(typeof User_Profile.nguoiYeu);
+console.log(typeof User_Profile.address);
